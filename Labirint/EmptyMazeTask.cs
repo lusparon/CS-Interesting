42051f35b62e7a339b93b654d3a964e2b462418c
lusparon <41898737+lusparon@users.noreply.github.com>
2019-04-10 00:16:14 +0300

    Добавил

---
+++ Labirint/EmptyMazeTask.cs
@@ -0,0 +1,13 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            for (int i = 0; i < height - 2; i++)
+                robot.MoveTo(Direction.Down);
+            for (int j = 0; j < width - 2; j++)
+                robot.MoveTo(Direction.Right);
+        }
+	}
+}
